076a8cf0744a2a35f28cd2023d54df2c8220d9cd
David Mazzitelli <davidma1984@gmail.com>
2022-05-25 01:43:24 -0300

    Fixed table name

--- src/models/wallet.model.ts
+++ src/models/wallet.model.ts
@@ -1,7 +1,7 @@
 import { Table, Column, Model, AllowNull, Unique } from 'sequelize-typescript';
 import { IsEthereumAddress } from 'class-validator';
 
-@Table({ modelName: 'wallets', timestamps: true })
+@Table({ modelName: 'Wallets', timestamps: true })
 export default class Wallet extends Model<Wallet> {
   @IsEthereumAddress()
   @AllowNull(false)
