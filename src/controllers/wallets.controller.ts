54846bf8b3298e9d921b8ba18bfdddb4728084ff
David Mazzitelli <davidma1984@gmail.com>
2022-05-25 03:08:54 -0300

    Added endpoints for wallet

--- src/controllers/wallets.controller.ts
+++ src/controllers/wallets.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Put,
+  Body,
+  UseInterceptors,
+  Param,
+  ParseIntPipe,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { WalletErrorInterceptor } from 'src/interceptors/wallet-error.interceptor';
 import { Wallet } from '../models';
 import { WalletsService } from '../services/wallets.service';
@@ -8,8 +18,28 @@ import { WalletsService } from '../services/wallets.service';
 export class WalletsController {
   constructor(private walletsService: WalletsService) {}
 
+  @Get()
+  findAll(@Query() query) {
+    return this.walletsService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.walletsService.findOne(id);
+  }
+
   @Post()
   create(@Body() params: Wallet) {
     return this.walletsService.create(params);
+  }
+
+  @Put('favorite/:id')
+  favorite(@Param('id', ParseIntPipe) id: number) {
+    return this.walletsService.updateFavorite(id, true);
+  }
+
+  @Put('unfavorite/:id')
+  unfavorite(@Param('id') id: number) {
+    return this.walletsService.updateFavorite(id, false);
   }
 }
--- src/services/wallets.service.ts
+++ src/services/wallets.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import Wallet from '../models/wallet.model';
 
 @Injectable()
 export class WalletsService {
-  async create(params): Promise<object> {
+  async findAll(query: any) {
+    // TODO: use query to sort
+    const wallets = await Wallet.findAll();
+    return wallets.map((wallet) => wallet.toJSON());
+  }
+
+  async findOne(id: number) {
+    const wallet = await this.getWallet(id);
+    return wallet.toJSON();
+  }
+
+  async create(params: Wallet) {
     const wallet = await Wallet.create(params);
     return wallet.toJSON();
+  }
+
+  async updateFavorite(id: number, isFavorite: boolean) {
+    const wallet = await this.getWallet(id);
+    wallet.update({ isFavorite: isFavorite });
+    return wallet.toJSON();
+  }
+
+  private async getWallet(id: number) {
+    const wallet = await Wallet.findOne({ where: { id: id } });
+    if (!wallet) {
+      throw new UnprocessableEntityException('Wallet ID not found');
+    }
+    return wallet;
   }
 }
